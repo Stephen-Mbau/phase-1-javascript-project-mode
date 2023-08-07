189ebc20be45ef4852b70753c8b501fb649e693b
Stephen Mbau <mbaustephen@gmail.com>
2023-08-07 22:18:58 +0300

    Done Project

---
+++ index.js
@@ -0,0 +1,60 @@
+const registerDetails = document.querySelector("#add");
+registerDetails.addEventListener("submit", function (event){
+    event.preventDefault()
+
+    console.log("successfully registered");
+});
+function buttonClicked () {
+    document.getElementById('button1').addEventListener('click', function() {
+        alert('Thank you for your order!');
+      });
+      
+      document.getElementById('button2').addEventListener('click', function() {
+        alert('Thank you for your order!');
+      });
+      
+      document.getElementById('button3').addEventListener('click', function() {
+        alert('Thank you for your order!');
+      });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    buttonClicked();
+  });
+ 
+function getDogImage() {
+    const dogImageElement = document.getElementById('dogImage');
+    const adoptButton = document.getElementById('adoptButton');
+    const url = 'https://dog.ceo/api/breeds/image/random';
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const dogImageURL = data.message;
+          dogImageElement.src = dogImageURL;
+        })
+        .catch(error => {
+          console.error('Error fetching pic', error);
+        });
+    function adoptDog() {
+            alert('Thank you for considering adopting this adorable dog!');
+        }
+
+    adoptButton.addEventListener('click', function() {
+            adoptDog();
+        });
+    }
+
+document.addEventListener('DOMContentLoaded', function() {
+    getDogImage()
+})
+const reviewForm = document.getElementById('consumer-review');
+reviewForm.addEventListener('submit',event=>{
+    event.preventDefault()
+    const text =document.getElementById('review');
+    const input = text.value;
+    const node = document.createElement('li');
+    const textNode = document.createTextNode(input);
+    node.appendChild(textNode);
+    document.getElementById('reviewList').appendChild(node);
+})
+
+
